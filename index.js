abf901ea6361d6092d81a1ee28ded96225213a94
kairuku <kairuku@pop-os.localdomain>
2023-05-04 17:44:48 -0700

    Change span to text input and update validation.

--- index.js
+++ index.js
@@ -13,28 +13,56 @@ let wordList = [];
 
 // Key handler
 const keyHandler = (e) => {
-  // console.log(e, e.keyCode, e.key);
+  console.log(e, e.keyCode, e.key);
   // If placeholder hasn't been reset yet or I guess technically if the user types or pastes this in there somehow
-  if (inputBox.innerText === "Type or click") {
-    inputBox.innerText = "";
-  }
+  // if (inputBox.innerText === "Type or click") {
+  //   inputBox.innerText = "";
+  // }
   // If keypress is a valid apzA-Z character, set span text
-  if (e.keyCode > 64 && e.keyCode < 91) {
-    inputBox.innerText += e.key.toUpperCase();
+  // if (e.keyCode > 64 && e.keyCode < 91) {
+  //   inputBox.value += e.key.toUpperCase();
+  // }
+  if (e.keyCode > 64 && e.keyCode < 91 || e.keyCode === 8) {
+    return true;
   }
-  // If keyCode is 8 (Backspace), remove last character
-  else if (e.keyCode === 8) {
-    let str = inputBox.innerText;
-    str = str.slice(0, -1);
-    inputBox.innerText = str;
-  }
-  // If keyCode is 13 (Enter), submit the word
   else if (e.keyCode === 13) {
+    console.log("Enter");
     // Submit word
-    submitWord(inputBox.innerText);
-    // Clear text
-    inputBox.innerText = "Type or click";
+    submitWord(toTitleCase(inputBox.value));
+    inputBox.value = "";
   }
+  else {
+    // return false;
+    e.preventDefault();
+  }
+
+  // If keyCode is 8 (Backspace), remove last character
+  // else if (e.keyCode === 8) {
+  //   let str = inputBox.innerText;
+  //   str = str.slice(0, -1);
+  //   inputBox.innerText = str;
+  // }
+  // If keyCode is 13 (Enter), submit the word
+  // else if (e.keyCode === 13) {
+  //   // Submit word
+  //   submitWord(inputBox.innerText);
+  //   // Clear text
+  //   inputBox.innerText = "";
+  //   inputBox.innerText = "Type or click";
+  // }
+}
+
+const formatInput = () => {
+  inputBox.value = inputBox.value.toUpperCase();
+}
+
+const toTitleCase = (str) => {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
 }
 
 const submitWord = (word) => {
@@ -54,4 +82,14 @@ const submitWord = (word) => {
 }
 
 // Add listener to element
-inputBox.addEventListener("keydown", keyHandler, true);
+// inputBox.addEventListener("keypress", keyHandler);
+// Add listerner, capitalize input
+inputBox.addEventListener("keyup", formatInput);
+
+/*
+  Letter list
+  Starting at top hex and going clockwise.
+  Last character is the center yellow required hex.
+  1. Y O B N A U T
+  2. V E L O A T G
+*/
